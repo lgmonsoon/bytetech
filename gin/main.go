88a1cdeee8453579970fbcfcba432599a6af1b9f
lgmonsoon <lizijie1106@outlook.com>
2023-02-04 18:08:54 +0800

    2/4/2023

---
+++ gin/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{})
+	})
+
+	r.Run(":8888")
+}
